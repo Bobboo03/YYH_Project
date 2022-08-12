ffae79c3433efa40cd79accf8ba6ed496ca7e094
yyh <dkemfdlfkdpr@naver.com>
2022-08-13 00:05:21 +0900

    commit

---
+++ MYJDM/script/img.js
@@ -0,0 +1,21 @@
+var index = 0;
+window.onload = function() {
+    slideshow();
+}
+
+function slideshow() {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    for(i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    index++;
+
+    if(index > x.length) {
+        index = 1;
+    }
+
+    x[index - 1].style.display = "block";
+    setTimeout(slideshow, 1000);
+
+}
